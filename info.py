9088ddd65872538d98e8925ed3f221ee6f3bfc75
Ganeshkarale212002 <98474751+Ganeshkarale212002@users.noreply.github.com>
2023-09-18 21:12:57 +0530

    Update info.py

--- info.py
+++ info.py
@@ -42,7 +42,7 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://jennie/?retryWrites=true&w=majority")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Premium:212002@premium.90drthe.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Premium")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Premium')
 # Others
