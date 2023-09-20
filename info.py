724a59f8aa3c737d92e053c9ca299780acdc2316
Ganeshkarale212002 <98474751+Ganeshkarale212002@users.noreply.github.com>
2023-09-20 17:18:09 +0530

    Update info.py

--- info.py
+++ info.py
@@ -42,9 +42,9 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Premium:212002@premium.90drthe.mongodb.net/?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "Premium")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Premium')
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Testing:212002@testing.7plrokt.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_NAME = environ.get('DATABASE_NAME', "Testing")
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Testing')
 # Others
 VERIFY = bool(environ.get('VERIFY', False))
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'easysky.in')
